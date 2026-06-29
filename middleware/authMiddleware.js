95875b07b2e845a64804098efd6fc1b6dde8b767
anish <anishkushwaha775@gmail.com>
2026-06-29 16:40:59 +0530

    file add

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({
+                success: false,
+                message: "Token Not Available"
+            });
+        }
+
+        // Example:
+        // Authorization: Bearer eyJhbGciOiJIUzI1NiIs...
+
+        const token = authHeader.split(" ")[1];
+
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+        const user = await User.findById(decoded.id).select("-password");
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid Token"
+            });
+        }
+
+        req.user = user;
+        next();
+
+    } catch (err) {
+        console.log("Some error in validating:", err);
+
+        return res.status(401).json({
+            success: false,
+            message: "Unable to Validate Token"
+        });
+    }
+};
+
+module.exports = authMiddleware;
